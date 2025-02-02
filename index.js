b18c85e0b2106d4efef075933d44ca789a467647
Eng-Wardheera <asadwarfaa@gmail.com>
2025-02-02 11:05:42 +0300

    book_store_app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectToDb from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -13,6 +14,7 @@ const port = process.env.PORT || 7000;
 app.use(express.json());
 app.use('/api/users/', userRoutes)
 app.use('/api/products/', productRoutes)
+app.use('/api/cart/', cartRoutes)
 
 app.listen(port, ()=>{
     console.log(`Server is Running on port ${port}`);
