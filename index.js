5ee2ce75df5622d6798488b8fd396e9ead56a8f1
Eng-Wardheera <asadwarfaa@gmail.com>
2025-02-05 05:38:56 +0300

    book_store_app

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 7000;
 app.use(express.json());
 app.use('/api/users/', userRoutes)
 app.use('/api/products/', productRoutes)
-app.use('/api/cart/', cartRoutes)
+app.use('/api/cart', cartRoutes)
 
 app.listen(port, ()=>{
     console.log(`Server is Running on port ${port}`);
