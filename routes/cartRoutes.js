34da69f272a948c46aa60db51dafe5fb0e4603a1
Eng-Wardheera <asadwarfaa@gmail.com>
2025-03-15 08:59:21 +0300

    first commit

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,6 +3,8 @@ import { addToCarts, addToWishlist } from '../controller/cartController.js';
 
 const router = express.Router();
 
+
+
 router.route('/').post(addToCarts);
 
 router.route('/add-to-wishlist').post(addToWishlist);
